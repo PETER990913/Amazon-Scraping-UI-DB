07207c055a7b8788371d4857ad5003d3c4fc0d13
PETER990913 <jeremy.t.1132@gmail.com>
2023-10-29 14:48:38 -0700

    'update518'

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-import json
-with open('musicalinstrument.json', 'r') as file:
-    for line in file:
-        # Do something with line1 and line2
-        line_word = line.strip()
-        print(line_word)
-        # item = json.loads(line_word)                 
-        if(line_word == "back"):
-            pass   
-        else:
-            item = json.loads(line_word)
-            item_text = item['name']
-            item_link = item['link'] 
-            print('item_text:', item_text) 
-            print('item_link:', item_link) 
-file.close()
+from selenium import webdriver
+
+# Create a ChromeOptions instance
+options = webdriver.ChromeOptions()
+
+# Replace this with the actual path to your Chrome user profile directory
+options.add_argument("user-data-dir=C:\\Users\\JSGURU\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 9")
+
+# Disable the "DevToolsActivePort" check, as it can sometimes cause issues
+options.add_argument("--disable-dev-shm-usage")
+
+# Disable the "sandbox" mode if you encounter issues
+options.add_argument("--no-sandbox")
+
+# Create a Chrome webdriver instance with the options
+driver = webdriver.Chrome(options=options)
+
+# Open a website
+driver.get("https://google.com")
+
+while True:
+    pass
